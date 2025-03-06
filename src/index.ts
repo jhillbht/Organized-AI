73f76c5f87e397dcc52561ee3a49549ca7052208
jhillbht <71741076+jhillbht@users.noreply.github.com>
2025-03-06 12:38:55 -0600

    Add index exports file

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+// Main application class
+export * from './OrganizedAI';
+
+// Core client
+export * from './client/OrganizedAIClient';
+
+// Types
+export * from './types';
+export * from './types/settings';
+
+// Managers
+export * from './settings/SettingsManager';
+export * from './session/SessionManager';
+
+// Utilities
+export * from './utils/logger';
+
+// Errors
+export * from './errors/ClientError';
